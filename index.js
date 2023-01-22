9dcd6649c0c01dfb998696f0fab48f57863602cb
Kasper 369 <mohsinakrambukhari@gmail.com>
2023-01-22 20:22:42 +0500

    changed advice to advices

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("express")();
-const advice = require("./advice.json");
+const advices = require("./advices.json");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the api, please so to /advice");
@@ -7,8 +7,8 @@ app.get("/", (req, res) => {
 
 
 app.get("/advice", (req, res) => {
-    const randomNo = Math.floor(Math.random() * advice.advice.length);
-  res.json({ advice: advice.advice[randomNo] });
+    const randomNo = Math.floor(Math.random() * advices.advices.length);
+  res.json({ advice: advices.advices[randomNo] });
 });
 
 app.listen(3000, () => console.log(`http://localhost:3000`));
